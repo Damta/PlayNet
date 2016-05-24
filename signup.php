911b483465a4aa8630f5c25b9332a9eaa3628a5e
Damta <fortestdamta@gmail.com>
2016-05-24 19:18:19 +0200

    Sign up system added

--- signup.php
+++ signup.php
@@ -15,54 +15,55 @@ catch(PDOException $e)
 {
      echo $e->getMessage();
 }
-
-$uname = trim($_POST['name']);
+  $uname = trim($_POST['name']);
+  $uuser = trim($_POST['username']);
   $umail = trim($_POST['email']);
   $upass = trim($_POST['password']);
   $ucheckpass = trim($_POST['confirm']);
-  if($uname=="") {
-     $error[] = "provide username !";
+  if($uuser=="") {
+     $error = "provide username !";
      echo $error;
   }
   else if($umail=="") {
-     $error[] = "provide email id !";
+     $error = "provide email id !";
      echo $error;
   }
   else if(!filter_var($umail, FILTER_VALIDATE_EMAIL)) {
-     $error[] = 'Please enter a valid email address !';
+     $error = "Please enter a valid email address !"  ;
      echo $error;
   }
   else if($upass=="") {
-     $error[] = "provide password !";
+     $error = "provide password !";
      echo $error;
   }
   else if(strlen($upass) < 6){
-     $error[] = "Password must be atleast 6 characters";
+     $error = "Password must be atleast 6 characters";
      echo $error;
   }
   else if($upass != $ucheckpass){
-    $error[] = "Passwords dont match!";
+    $error = "Passwords dont match!";
     echo $error;
   }
+  else
   {
      try
      {
         $stmt = $DB_con->prepare("SELECT * FROM users WHERE username=:uname OR email=:umail");
-        $stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
+        $stmt->execute(array(':uname'=>$uuser, ':umail'=>$umail));
         $row=$stmt->fetch(PDO::FETCH_ASSOC);
 
         if($row['user_name']==$uname) {
-           $error[] = "sorry username already taken !";
+           $error = "sorry username already taken !";
         }
         else if($row['user_email']==$umail) {
-           $error[] = "sorry email id already taken !";
+           $error = "sorry email id already taken !";
         }
         else
         {
-           if($user->register($fname,$lname,$uname,$umail,$upass))
-           {
-               $user->redirect('sign-up.php?joined');
-           }
+            $createuser = $DB_con->prepare("INSERT INTO `users` (`name`, `email`, `username`, `password`) VALUES ('$uname', '$umail', '$uuser', '$upass')");
+            $createuser->execute();
+            echo "ddfds";
+
         }
     }
     catch(PDOException $e)
